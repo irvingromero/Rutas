2256f24cc5d27a3008f372144a76a9dd9cc42c0e
Irving <i.romerog93@gmail.com>
2020-03-26 14:57:22 -0700

    v 1.5 - Ruta 9 agregada - Ruta Eje agregada - Ruta Nacionalista agregada

--- app/src/main/java/com/irv/rutas/MapsActivity.kt
+++ app/src/main/java/com/irv/rutas/MapsActivity.kt
@@ -47,7 +47,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private var chipgroup: ChipGroup? = null
     private var btnRutas: MaterialButton? = null
 
-    var poly : ArrayList<LatLng>? = null
+    private var poly: ArrayList<LatLng>? = null
+    private val rutaPuntos: ArrayList<String> = ArrayList()
+    private var points: ArrayList<LatLng>? = null
+    private var polylineOptions: PolylineOptions? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -73,15 +76,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             ventanaRutas()
         }
 
-            ///// RUTAS /////
+        ///// RUTAS /////
         listaRutas = ArrayList()
         listaRutas?.add("Ruta 9")
         listaRutas?.add("Nacionalista")
+        listaRutas?.add("Eje")
+//        listaRutas?.add("Ruta 4")
         listaRutas?.sort() //ORDENA ALFABETICAMENTE //
     }
 
     private fun menuSlide() {
         drawerLayout?.addDrawerListener(object : ActionBarDrawerToggle(this, drawerLayout, 0, 0) {
+
             override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
                 super.onDrawerSlide(drawerView, slideOffset)
                 val slideX = drawerView.width * slideOffset
@@ -194,7 +200,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun chipRuta(vista : View, ruta : String) {
+    private fun chipRuta(vista: View, ruta: String) {
         val i = LayoutInflater.from(this)
         val chipItem = i.inflate(R.layout.chip, null, false) as Chip
 
@@ -235,8 +241,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun decodePoly(encoded : String): ArrayList<LatLng>? {
-        poly  = ArrayList()
+    private fun decodePoly(encoded: String): ArrayList<LatLng>? {
+        poly = ArrayList()
         var index = 0
         val len = encoded.length
         var lat = 0
@@ -271,25 +277,132 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         when (ruta) {
             "Ruta 9" -> ruta9()
             "Nacionalista" -> nacionalista()
+            "Eje" -> eje()
         }
     }
 
-    private fun ruta9(){
-        val ruta = "cdifEn_b`UbC@vCEfC?vDBtAAtA?`E@tL?tKHdE?xPBfD?`DB`H?tN@jLBvCAR?FFD@`@H|@Xf@BpBA~A?vDDF?pAG|Ba@b@CLEfA@fBBxAC|DDz@FfB`@l@\\|@f@`An@d@Rp@Pp@Jn@BbB?`AHrATj@JdBFnIA|@A"
+    private fun ruta9() {
+        // OXXO LUCERNA A CETIS 18 //
+        rutaPuntos.add("cdifEn_b`UbC@vCEfC?vDBtAAtA?`E@tL?tKHdE?xPBfD?`DB`H?tN@jLBvCAR?FFD@`@H|@Xf@BpBA~A?vDDF?pAG|Ba@b@CLEfA@fBBxAC|DDz@FfB`@l@\\|@f@`An@d@Rp@Pp@Jn@BbB?`AHrATj@JdBFnIA|@A")
+        // DE CETIS 18 A GLORIETA //
+        rutaPuntos.add("{lbfEthb`UT??aF@wSFub@?qESuCWiB]sAu@eCoCwIoA{Dq@qCOiACu@")
+        // GLORIETA A TERAN TERAN //
+        rutaPuntos.add("aybfErz~_UJWH_AJQV_@d@Q~@Kd@KJKN?|A?`B?|BJ|@L`BZdAX|An@^NIo@?_@BULWZKnKGfC?lD?tFEnD?")
+        // TODA TERAN TERAN HASTA COSTCO //
+        rutaPuntos.add("ux`fEjt~_UX@@s@FqLAeFEgI?wD@_EAgTBcKEaMHuOJ}CNmBPkBd@{Ch@{D^cD`Fa[n@mDb@eBvDkNjDsL^}Af@eBzAkFpBaH`@}At@kCr@wCh@oCRgBJsC\\oPj@aYh@_XTyKXkKRuJFqE`@uMFmDLuFBs@HkA\\eCXiAf@wAf@aAr@kAbA}A|@aBd@uANy@Fs@@k@AyAGy@i@mDm@{Dw@sEc@qAcFgH_E}FW[ED")
+        // DE COSTCO A PEDREGAL //
+        rutaPuntos.add("in_fErep_UIHVZjPwRtCuDnEoG~AoCvAcC|@uBnCcFjEwJtC{HrCiJ|C_M`CuJpDmNxD_PfEqPxBaJhG_Wj@oBj@iCjDyM`@kA`@gAT{@f@oBN}@|BeJt@gEhBiHhAeEnBeI|BmJxBuJj@wDj@yDzByJfCcKrBiIxK}a@hDsNxB{IpAaFbEaPtAoFxCwLBQ")
+        // CALZADA ROSA DEL DESIERTO (PEDREGAL) //
+        rutaPuntos.add("wvyeEvcb_U@E]MqDyAaJkDo@]y@s@iBgBiAeAo@c@eAe@}C_A}DqAmBk@qC_AaEqAeB_AeAs@eC}BkAaA")
+        // TODA LA LAZARO CARDENAS DESDE PEDRAGAL HASTA GLORIETA //
+        rutaPuntos.add("wy{eEn|`_U_@]g@c@OVgMrTyBzDgBzCsBjDgD|FmB~CqDvGcMhTeIlNoCzE{BtDmHrM_BxCmD~FwDvG}KtRmItNqM`U{CrF_@n@iHzLgDbGsE`Io@nA_BfCiBbDqBhDmCvEmQ|Z{@zAeFzIWh@_A|AuBvDcAlBMZq@xA{@xAc@f@iA~AkAlB_BdDc@pAg@nBWjBQtAIfAGpB?~BH`CJ~Q?bD?dJCxFBhGDrN@bLD`PCbDCxCB|CBzCDtJFnIPra@H~J")
+        // GLORIETA LAZARO CARDENAS Y JUSTO SIERRA A CALLE INDEPENDENCIA //
+        rutaPuntos.add("k_cfE~dr_U?NEPCRAlEAf@Gn@APGDIPg@`@a@X]PoJlCaFrA}DhA}ErAuBf@eEjAkBd@uCz@_Cn@kBj@kKlCsCz@PbA")
+        // TODA LA CALLE INDEPENDENCIA HASTA CALLE DEL HOSPITAL//
+        rutaPuntos.add("cmefErqs_UpBbLx@~ETx@GXBb@LjBDjA?nASnIAl@DPGpCWbJI`D@rGU|JIrBQvA[fEM~DI`ACr@OfGM`BM`A[bB[lAwApDs@dAQj@}@rAcGdI_BzBaBxBkEzFeFbHgArA")
+        // CALLE DEL HOSPITAL HASTA LOPEZ MATEOS//
+        rutaPuntos.add("wsffEdby_UgDk@SGc@U_BeBwCoEk@q@oFkFiAaAe@]iC}AmA}@s@k@MMW_@EIJE")
+        // TODA LOPEZ MATEOS HASTA ALTAMIRANO -CENTRO //
+        rutaPuntos.add("}xgfEbax_U{G`Cs@PYAu@Pa@Ta@h@oAvAg@`@iBhAyCbCmBbB{B`CoArAm@v@mBnCaCtDqA|BqAlCeAfCkA|CiB`FwA~DiG~QmFzOkF`Ph@?F?")
+        // CALLE ALTAMIRANO HASTA MICHOACAN //
+        rutaPuntos.add("_ljfEx{{_U~DAlGElG?xBEh@rCh@rC`@bAjApDJ\\lBA~A@")
+        // TODA LA MICHOACAN //
+        rutaPuntos.add("mdifEfm|_UL@@~F@~N?`OA~NA~N@l^DzF?hGA`L?fE?jL?|L@rD@fEM?")
 
-        var points: ArrayList<LatLng>? = null
-        var polylineOptions : PolylineOptions? = null
+        rutaPuntos.forEach {
+            points = decodePoly(it)
 
-        points = decodePoly(ruta)
+            polylineOptions = PolylineOptions()
+            polylineOptions?.color(Color.RED)
+            polylineOptions?.width(7.5f)
+            polylineOptions!!.addAll(points)
+            mMap.addPolyline(polylineOptions)
+        }
 
-        polylineOptions = PolylineOptions()
-        polylineOptions.color(Color.RED)
-        polylineOptions.addAll(points)
-        mMap.addPolyline(polylineOptions)
-        mMap.addPolyline(polylineOptions)
+        rutaPuntos.clear()
+        points?.clear()
+        poly!!.clear()
     }
 
-    private fun nacionalista(){
+    private fun nacionalista() {
+        // DESDE EL CENTRO (SALIDA) A CALLE DEL HOSPITAL TODA LA LOPEZ MATEOS//
+        rutaPuntos.add("crjfEhi|_UrImWnH}ThDcKl@cB^mAh@yAhCcHtCuHj@oAnBoDpBaDfAaBzBwCdCmCbD}CzAqAlDsCfBmAr@_@VEd@STO\\c@xA_AzB}@fCgA")
+        // CALLE DEL HOSPITAL //
+        rutaPuntos.add("cxgfE|bx_Uf@n@nA|@lBpApAv@z@n@jGdGd@f@jAdBxAzBjApAJJXRZL`@JnCb@")
+        //  INDEPENDENCIA! //
+        rutaPuntos.add("wsffEdby_U`BuBpFsHvFoH~CmEfFaHl@}@HCNIDE")
+        // ANAHUAC //
+        rutaPuntos.add("urefEhyw_UHKT~@|@zDTx@Zf@t@`@hEjBtGbCrB~@tAdAfIpKxAlBl@p@XVjAx@fAd@rBj@x@NXT`ANrCb@|Cf@~KjBzB\\x@Jh@\\XXV\\^|@Pf@?zA?dA")
+        // LAZARO CARDENAS HASTA GLORIETA //
+        rutaPuntos.add("gzbfE~zy_U?`U?pI?fL?lF@fF?vDCz_@B`p@KdBCDEFIT?\\HZFFHFXDVGHEBEXG|@Mf@ChA?p@?")
+        // CALZADA DEL SOL //
+        rutaPuntos.add("mpbfErt~_UlC?|BJ|@L`BZdAX|An@hBbAzBpBl@n@j@z@z@vA`EhJx@tBt@jBb@[Qc@")
+        // PAPAGO HASTA GLORIETA //
+        rutaPuntos.add("}gafExt_`UoBuEoA_DkBcEeBgCgAcAcA}@iBiAw@c@k@Wo@WkAWoB[iBKcBCkA?oFIOESAACACCCIKYGWDMJEFIT?\\HZPNFx@?~@@v@")
+        // DESDE GLORIETA (GALERIAS) A YUGOSLAVIA //
+        rutaPuntos.add("_zbfEl{~_UBr@RvAb@bBz@xCdA~C`B~EpAxEPpAPrBBlCD~BCfMHbS@tAE`K@jME?")
+        // YUGOSLAVIA //
+        rutaPuntos.add("ambfEthb`Uw@@oI@eBGk@KsAUaAIcB?o@Cq@Kq@Qe@SaAo@}@g@m@]gBa@{@GaBC{AAyABgBCgAAWEYAcCBqDA{DAuA?W?m@By@ASEQ?IHsB@qDAkG?uECwM?oPCcRA@\\FP")
+        // AVENIDA SANTA ISABEL //
+        rutaPuntos.add("}hgfEjab`UMrXN|J@jCHlQ")
+        // GOMEZ MORIN (PLAZA DEL SOL) //
+        rutaPuntos.add("ohgfEv}c`U?TYx@]t@OLm@RWD}AEkD@")
+        // AV VALLECITOS
+        rutaPuntos.add("gugfE`cd`UDgI?uLK?")
+        // CALZADA LUIS DONALDO COLOSIO MURRIETA//
+        rutaPuntos.add("mugfEbkc`U}M@{D?_BA?V")
+        // REAL DELCASTILLO //
+        rutaPuntos.add("gmhfEvkc`UBfFEfLCzAoC?wADgCG?P")
+        rutaPuntos.add("}xhfErcd`U?T|B?xB?jBCAhC")
+        // COLOMBAR Y MISION PIAROLA //
+        rutaPuntos.add("{mhfEnhd`UEfUlD?`@?@fL")
+        // COCOIT //
+        rutaPuntos.add("sghfE~ke`UoJ?{DA?l@")
+        // OAXACA //
+        rutaPuntos.add("_yhfEjme`U?zK?hJ?zMQl@?bB?|AE~K@zE")
 
+        rutaPuntos.forEach {
+            points = decodePoly(it)
+
+            polylineOptions = PolylineOptions()
+            polylineOptions?.color(Color.RED)
+            polylineOptions?.width(7.5f)
+            polylineOptions!!.addAll(points)
+            mMap.addPolyline(polylineOptions)
+        }
+
+        rutaPuntos.clear()
+        points?.clear()
+        poly!!.clear()
+    }
+
+    private fun eje(){
+        // ORIGEN(Progreso) A LAZARO CARDENAS //
+        rutaPuntos.add("ywzeEx}m`U_D}AcEyBaCkAsM{GeG_DaMqGu@a@gJ{E_CiAgIcEiDeBqC}A{EkDuAaAWMi@Se@Ig@CmBAig@HmA?oEAaPCoOAuB@qBPqBNmBDwQ?mLAiCYoCAWQ]e@AgA")
+        // INICIO DE LAZARO A COCA COLA FRENTE A PLAZA CARRANZA //
+        rutaPuntos.add("gnbfEd{k`U@qD?gS?sFC{AFwDDuJ@cD@yGC{DCgE?{F?sp@FaAAcBMuMCaIE_HAmGEgSN}ZHiIJsCTyMRoMFkGAkEAiH?iD?aRBkLDa^?qESuCWiB]sAu@eCc@yAgC_Is@yBq@qCOiACu@ByAFm@BUBABEJY@_@IUEGACG[I_A@kF?cOAaY?qGB}HAsKAcB@{FC_E?wPBmOAyBAgNAiC@eD?qK?oDAmA?sMBkDGyK@aE@{EBeJCaFA_J@{BEgBYsDGu@i@eIc@_Hu@iKScDB}A?W@i@\\{G?aDC{TEqIG_TAeUAsM@cMKkBCqKEqGAwECiE?mC?yDCeUGsYCiIO{LAg\\BoD")
+        // HASTA FINAL DE LAZARO CARDENAS //
+        rutaPuntos.add("m`cfExel_U@mBHqBP_Bh@_DVkAf@wAh@oAd@w@x@}Al@aA`AaB~A{Cx@gBj@kApAqBv@aA\\k@nBgDzAsC`BsCxA}BhCqE|@yAnBoDhAsBnEyH|D_HdGsKpJgPzHgN|So^tHuM~HiN`E_HbIsNfFkI~EwI~DcHjB}CtAeCzImOzMwUh@w@pBqDbGoKjI{Nn@_AfB{CvIeO|IsO")
+        rutaPuntos.add("{g{eE|~__Uf@{@f@m@x@m@v@[dAO|CEzBAlMWhJMjJGjEMVBrCC`DAf@Bb@HPLLNPf@Bf@Gt@kAbF^LdBbA~EtCzJ~FbI|E~ErCpMpHbDpB|G~D|AbAxGtDlC`B")
+        // AV CASCADA LA BALSA //
+        rutaPuntos.add("mjveEb|a_URLa@hB_CbKeA`Eu@|Ca@bBOfAYjCOv@cApE{CvMaBzHwBrIaAnDs@Y")
+        // CALLE TLAXCALTECAS //
+        rutaPuntos.add("cgweE~pd_UyDyAwCqA_DkAoAi@EP")
+        // AVENIDA TECAMACHALCO //
+        rutaPuntos.add("kyweEnhd_U{CrLyBrIy@lC")
+
+        rutaPuntos.forEach {
+            points = decodePoly(it)
+
+            polylineOptions = PolylineOptions()
+            polylineOptions?.color(Color.RED)
+            polylineOptions?.width(7.5f)
+            polylineOptions!!.addAll(points)
+            mMap.addPolyline(polylineOptions)
+        }
+
+        rutaPuntos.clear()
+        points?.clear()
+        poly!!.clear()
     }
 }
